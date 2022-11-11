5e35cf7a5d81805d1a1403cde56affeca0e8582e
NobodyExpectsTheSpanishInquisition <s.miklaszewski1999@gmail.com>
2022-11-11 13:34:13 +0100

    refactor(auth): remove redundant test

--- api/auth/database/database_test.go
+++ api/auth/database/database_test.go
@@ -5,14 +5,6 @@ import (
 	"testing"
 )
 
-func TestCreateDatabaseConnection_ShouldPass_WhenInvalidDsnParametersReadAndErrorReturned(t *testing.T) {
-	_, err := CreateDatabaseConnection(config.Reader{})
-
-	if nil == err {
-		t.Fail()
-	}
-}
-
 func TestCreateDatabaseConnection_ShouldReturnDatabaseConnection_WhenCorrectDsnParametersRead(t *testing.T) {
 	reader, _ := config.InitConfigReader()
 	_, err := CreateDatabaseConnection(reader)
